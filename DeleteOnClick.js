7e5117810f0cacf96b65a1d53feff4905b7f3855
Oliver <63563918+CatR3kd@users.noreply.github.com>
2023-03-01 14:55:31 -0600

    Delete DeleteOnClick.js

--- DeleteOnClick.js
+++
@@ -1,9 +0,0 @@
-// Deletes every element the user clicks
-for(let element of document.getElementsByTagName("*")){
-  element.addEventListener('click', function(event){
-    if(event.target == element){
-      event.preventDefault();
-      element.remove();
-    }
-  });
-}
